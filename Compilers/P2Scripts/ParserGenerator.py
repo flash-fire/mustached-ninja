e447567e4fe4b12aa06231a173d17233c1c5a3d8
flash-fire <packerkid2@yahoo.com>
2015-01-02 16:57:43 -0600

    LET THE HAMMER OF REFACTORING CONTINUE
    yummyy

--- Compilers/P2Scripts/ParserGenerator.py
+++ Compilers/P2Scripts/ParserGenerator.py
@@ -1,103 +1,88 @@
 from MassageGrammar import *
 from FirstFollows import *
 
-# First of a single token  
-def buildParser(nts, productions):
+# First of a single token   
+def writeParser():
    first_dict = firsts(nts, productions)
    follows_dict = follows(nts, productions, first_dict)
+   def wizardPowers(nt):
+      # Here we handle a given nonterminal's method for the parser
+      outStr = "void Project2::" + nt + "() {\n" # declaration
+      outStr += "\tstd::string nt = \"" + nt + "\";\n" # make nt
+      outStr += "\tstd::string debugLEX = lookAhead.lex;\n"
+      outStr += "\tint debugLineNum = lookAhead.line+1;\n"
+      outStr += "\tint debugCharCol = lookAhead.charNum+1;\n"
+      if '' in first_dict[nt]:
+         exp = (first_dict[nt] | follows_dict[nt]) - {''}
+      else:
+         exp = first_dict[nt]
+      expout = ""
+      for x in exp:
+         expout = expout + x + " "
+      expout = expout[:-1]
+      outStr += "\tstd::string exp = \"" + expout + "\";\n" # make expected
+      
+      for prods in productions[nt]:
+         firsts = firstTokStream(prods, nts, productions, first_dict)
+         firstsLs = list(firsts)
+         for i in range(0, len(firstsLs)):
+            if '' in firsts:
+               outStr += "\n\tif("
+               followsLs = list(follows_dict[nt])
+               for j in range(0, len(followsLs)):
+                  if j == len(followsLs) -1:
+                     outStr += "lookAhead.token == p->GTT(\"" + followsLs[j] + "\")) {\n"
+                     outStr += "\t\treturn;\n"
+                     outStr += "\t}\n" #close if
+                  else:
+                     outStr += "lookAhead.token == p->GTT(\"" + followsLs[j] + "\") || "                  
+            else:
+            
+               checkStr = firstsLs[i]
+               #outStr += "\tstd::string curr = \"" + firstsLs[i] + "\";\n"
+               outStr += "\n\tif("
+               outStr += "lookAhead.token == p->GTT(\"" + checkStr + "\")) {\n" # if statements
+               for targ in prods:
+                  if targ in nts:
+                     outStr += "\t\t" + targ + "();\n"
+                  else:
+                     outStr += "\t\tMatch(p->GTT(\"" + targ + "\") ,nt, \"" + targ + "\");\n"
+               outStr += "\t\treturn;\n" # essentially a break in a case statement when using ifs
+               outStr += "\t}\n" # close if
+      outStr += "\tSynErrorTok(nt, exp);\n"
+      outStr += "}\n\n" # close method
+      return outStr
+
+   # Here we write the parser
+   p = "parser/"
+   d = "data/"
+   synch     = open(p+"synch.txt","w") # Writes synchset
+   parseboil = open(d+"ParserBoilerCode.txt","r")
+   headStart = open(d+"ParserHeaderStart.txt","r")
+   headEnd   = open(d+"ParserHeaderEnd.txt","r")
+   pout = open(p+"Parser.cpp",'w')
+   hout = open(p+"Parser.h",'w')
+
+   copyFile(parseboil, pout)
+   copyFile(headStart, hout)
    
-   def writeParser():
-      def wizardPowers(nt):
-         # Here we handle a given nonterminal's method for the parser
-         outStr = "void Project2::" + nt + "() {\n" # declaration
-         outStr += "\tstd::string nt = \"" + nt + "\";\n" # make nt
-         outStr += "\tstd::string debugLEX = lookAhead.lex;\n"
-         outStr += "\tint debugLineNum = lookAhead.line+1;\n"
-         outStr += "\tint debugCharCol = lookAhead.charNum+1;\n"
-         if '' in first_dict[nt]:
-            exp = (first_dict[nt] | follows_dict[nt]) - {''}
-         else:
-            exp = first_dict[nt]
-         expout = ""
-         for x in exp:
-            expout = expout + x + " "
-         expout = expout[:-1]
-         outStr += "\tstd::string exp = \"" + expout + "\";\n" # make expected
-         
-         for prods in productions[nt]:
-            firsts = firstTokStream(prods, nts, productions, first_dict)
-            firstsLs = list(firsts)
-            for i in range(0, len(firstsLs)):
-               if '' in firsts:
-                  outStr += "\n\tif("
-                  followsLs = list(follows_dict[nt])
-                  for j in range(0, len(followsLs)):
-                     if j == len(followsLs) -1:
-                        outStr += "lookAhead.token == p->GTT(\"" + followsLs[j] + "\")) {\n"
-                        outStr += "\t\treturn;\n"
-                        outStr += "\t}\n" #close if
-                     else:
-                        outStr += "lookAhead.token == p->GTT(\"" + followsLs[j] + "\") || "                  
-               else:
-               
-                  checkStr = firstsLs[i]
-                  #outStr += "\tstd::string curr = \"" + firstsLs[i] + "\";\n"
-                  outStr += "\n\tif("
-                  outStr += "lookAhead.token == p->GTT(\"" + checkStr + "\")) {\n" # if statements
-                  for targ in prods:
-                     if targ in nts:
-                        outStr += "\t\t" + targ + "();\n"
-                     else:
-                        outStr += "\t\tMatch(p->GTT(\"" + targ + "\") ,nt, \"" + targ + "\");\n"
-                  outStr += "\t\treturn;\n" # essentially a break in a case statement when using ifs
-                  outStr += "\t}\n" # close if
-         outStr += "\tSynErrorTok(nt, exp);\n"
-         outStr += "}\n\n" # close method
-         return outStr
-
-      # Here we write the parser
-      p = "parser/"
-      d = "data/"
-      synch     = open(p+"synch.txt","w") # Writes synchset
-      parseboil = open(d+"ParserBoilerCode.txt","r")
-      headStart = open(d+"ParserHeaderStart.txt","r")
-      headEnd   = open(d+"ParserHeaderEnd.txt","r")
-      pout = open(p+"Parser.cpp",'w')
-      hout = open(p+"Parser.h",'w')
- 
-      copyFile(parseboil, pout)
-      copyFile(headStart, hout)
-      
-      for nt in nts:
-         s = wizardPowers(nt)
-         pout.write(s)
-         hout.write("\tvoid Project2::" + nt + "();\n")
-      
-      copyFile(headEnd, hout)
-      
-      # Here we write the synch sets
-      synch_dict = {nt: follows_dict[nt] | {'$'} for nt in nts}
-      writeSynchSetFormat(synch, nts, synch_dict)
-      
-      synch.close()
-      parseboil.close()
-      headStart.close()
-      headEnd.close()
-      pout.close()
-      hout.close()
-     
    for nt in nts:
-      firstNT(nt, nts, productions)
-   for nt in nts:
-      followsOf(nt, nts, productions)
-   print("\n\n\n*** FIRSTS ***\n\n")
-   for nt in nts:
-      print (nt, first_dict[nt])
-   print("\n\n*** FOLLOWS ***\n\n")
-   for nt in nts:
-      print (nt, follows_dict[nt])
-   writeParser()
-
+      s = wizardPowers(nt)
+      pout.write(s)
+      hout.write("\tvoid Project2::" + nt + "();\n")
+   
+   copyFile(headEnd, hout)
+   
+   # Here we write the synch sets
+   synch_dict = {nt: follows_dict[nt] | {'$'} for nt in nts}
+   writeSynchSetFormat(synch, nts, synch_dict)
+   
+   synch.close()
+   parseboil.close()
+   headStart.close()
+   headEnd.close()
+   pout.close()
+   hout.close()
 
 if __name__ == '__main__':
    g="grammar/"
